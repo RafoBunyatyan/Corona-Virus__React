db7d6eead05b908f69c8c2cfe7c7585a87d54ceb
Rafo Bunyatyan <rafobuniatyan@mail.ru>
2022-02-18 19:46:38 +0400

    register

--- first-app3/src/App.js
+++ first-app3/src/App.js
@@ -1,9 +1,16 @@
-import React, { useContext } from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import './App.css';
+import Nav from './Components/Nav';
+import Profile from './Components/Profile';
 import Home from './Components/Home';
+import Login from './Components/Login';
+import Register from './Components/Register';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Theme from './Components/context';
 
 function App() {
+  const [showRegister, setShowRegister] = useState(true);
+  const [showValue, setShowValue] = useState(false);
   const { theme } = useContext(Theme);
 
   if (theme) {
@@ -14,9 +21,43 @@ function App() {
     document.body.style.color = "#fff";
   };
 
+  useEffect(() => {
+    setShowValue(!!localStorage.getItem("token"));
+  }, [showValue]);
+
+  const handleClick = () => {
+    setShowRegister(!showRegister);
+  };
+
   return (
     <>
-        <Home />
+      <Router>
+        <Nav
+          showValue={showValue}
+          showRegister={showRegister}
+          handleClick={handleClick}
+        />
+        <Switch>
+          <Route path="/" exact>
+
+          </Route>
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route path="/profile">
+            <Profile />
+          </Route>
+        </Switch>
+        {showRegister ? (
+          <Route path="/login">
+            <Login setShowValue={setShowValue} />
+          </Route>
+        ) : (
+          <Route path="/register">
+            <Register setShowValue={setShowValue} />
+          </Route>
+        )}
+      </Router>
     </>
   );
 };
--- first-app3/src/Components/Home.js
+++ first-app3/src/Components/Home.js
@@ -1,19 +1,11 @@
-import React, { useContext } from "react";
+import React, {  } from "react";
 import CoronaVirus from "./CoronaVirus";
-import Theme from "./context";
 
 function Home() {
-  const { theme, setTheme } = useContext(Theme);
-
   return (
     <>
       <h1>Home</h1>
       <CoronaVirus />
-	    <button color={ theme ? "dark" : "primary" }
-		  onClick={() => {
-            setTheme(!theme);
-          }}>{ theme ? "Dark" : "Ligth" }
-        </button>
     </>
   );
 };
